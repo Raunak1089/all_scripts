0d4a14410785a37c8c09e191759795a576e9dbee
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2022-02-09 18:38:35 +0530

    Update touch_functions.js

--- touch_functions.js
+++ touch_functions.js
@@ -1,14 +1,23 @@
 
-function two_touch_distance(e) {
+function two_touch_distance(e,first_value,last_value) {
       let x0 = e.touches[0].clientX - e.target.offsetLeft;
       let y0 = e.touches[0].clientY - e.target.offsetTop;
 
       let x1 = e.touches[1].clientX - e.target.offsetLeft;
       let y1 = e.touches[1].clientY - e.target.offsetTop;
 
-     let distance = Number(Math.floor(Math.sqrt(Math.pow((x0 - x1),2) + Math.pow((y0 - y1),2))));
-     return distance;
+     let distance = Number(Math.sqrt(Math.pow((x0 - x1),2) + Math.pow((y0 - y1),2)));
+     
+     
+var a = 40; var b = 720; var c = Number(first_value); var d = Number(last_value); 
+var s = (d - c) / (b - a) ;
+  
+let final_distance = Math.floor(c + s * (distance - a));
+
+return final_distance
+
      }
+
 
 
 
