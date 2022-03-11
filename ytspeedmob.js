10957fa7d47709f2459d07b9c0f8d1849cb298f9
Raunak Ray <raunakray1089@gmail.com>
2022-03-11 17:23:28 +0530

    PC done

--- ytspeedmob.js
+++ ytspeedmob.js
@@ -1,24 +1,26 @@
 var mydiv = document.createElement('div'); 
-mydiv.style.height = '40px';
-mydiv.style.width = 'fit-content';
-mydiv.style.background = 'grey';
-mydiv.style.borderRadius = '10px';
-mydiv.style.padding = '10px';
-mydiv.style.position = 'fixed';
-mydiv.style.opacity = 0.3;
-mydiv.style.transition = '0.5s';
-mydiv.style.flex = '1';
-mydiv.style.zIndex = '1000';
-mydiv.style.top = '50vh';
-mydiv.style.left = '5px';
 
+let css = ` 
+        height: 40px;
+        width: fit-content;
+        background: grey;
+        border-radius: 10px;
+        padding: 10px;
+        position: absolute;
+        opacity: 0.7;
+        flex: 1;
+        z-index: 1000;
+        margin-top: 300px;
+        user-select: none;
+` 
+mydiv.setAttribute("style", css); 
 
 var myans = document.createElement('span');
 myans.style.padding = '2px 10px';
 myans.style.background = 'white';
 myans.style.borderRadius = '50%';
 myans.style.fontSize = '30px';
-myans.innerHTML = '1x';
+myans.innerHTML = '1';
 
 
 mydiv.appendChild(myans);
@@ -27,25 +29,36 @@ document.body.appendChild(mydiv);
 
 mydiv.ontouchmove = function(ev) {
            var e = ev.targetTouches[0];
-           var speed = Math.floor(100*Math.pow(1.005, (e.screenY-300)))/100;
-           myans.innerHTML = speed+'x';
-           document.getElementsByClassName('html5-main-video')[0].playbackRate = speed;
+           myans.innerHTML = Math.floor(100*Math.pow(1.005, (e.pageY-300)))/100;
+           document.getElementsByClassName('html5-main-video')[0].playbackRate = eval(myans.innerHTML);
+           mydiv.style.transition = 'opacity 0.3s';
            }
 
+var dragValue;
 
+mydiv.onmousedown = function(){
+        dragValue = myans;
+
+document.onmouseup = function(){
+        dragValue = null;
+      }
+
+document.onmousemove = function(e) {
+        dragValue.innerHTML = Math.floor(100*Math.pow(1.005, (e.clientY-300)))/100;
+        document.getElementsByClassName('html5-main-video')[0].playbackRate = eval(dragValue.innerHTML);
+        mydiv.style.transition = 'opacity 0.3s';
+      };
+
+}
 
 var disablescroll = document.createElement('script');
 disablescroll.src = "https://raunak1089.github.io/all_scripts/disablescroll.js";
 document.body.appendChild(disablescroll);
 
     mydiv.ontouchstart = function() {
-        mydiv.style.opacity = 1;
-        mydiv.style.left = '40vw';
         disableScroll();
     }
 
     mydiv.ontouchend = function() {
-        mydiv.style.opacity = 0.5;
-        mydiv.style.left = '5px';
         enableScroll();
 }
