bd4c4443d82b3411412b0684564dadaef68c4c6e
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2022-12-02 17:23:27 +0530

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,5 @@
-  indic=['The','This','That'];
-  subject=['movie','webseries','film','episode','scene','serial'];
-  good=["breathtaking", "amazing", "stunning", "astounding", "astonishing", "awe-inspiring", "stupendous", "staggering", "extraordinary", "incredible", "unbelievable", "magnificent", "wonderful", "spectacular", "remarkable", "phenomenal", "prodigious", "miraculous", "sublime", "formidable", "imposing", "impressive", "mind-boggling", "mind-blowing", "out of this world", "amazeballs", "badass", "wondrous", "awful"]
+indic=['The','This','That'];
+subject=['movie','webseries','film','episode','scene','serial'];
+good=["breathtaking", "amazing", "stunning", "astounding", "astonishing", "awe-inspiring", "stupendous", "staggering", "extraordinary", "incredible", "unbelievable", "magnificent", "wonderful", "spectacular", "remarkable", "phenomenal", "prodigious", "miraculous", "sublime", "formidable", "imposing", "impressive", "mind-boggling", "mind-blowing", "out of this world", "amazeballs", "badass", "wondrous", "awful"]
+ok=['all right', 'right', 'right then', 'right you are', 'very well', 'yes', 'very good', 'fine', 'agreed', 'achcha', 'oke', 'okey-dokey', 'okey-doke', 'roger', 'righto', 'righty-ho'];
+
