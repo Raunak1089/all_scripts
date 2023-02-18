395664382ffab4f1106cb30572ace859b99acf80
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2023-02-18 18:04:05 +0530

    Update localspeed.js

--- localspeed.js
+++ localspeed.js
@@ -1,161 +1,146 @@
-    var fnt = document.createElement('style'); 
-    fnt.innerHTML="@import url('https://fonts.googleapis.com/css?family=Comic Neue:400,500,600,700&display=swap');";
-    document.body.appendChild(fnt); 
+var fnt = document.createElement('style'); 
+fnt.innerHTML="@import url('https://fonts.googleapis.com/css?family=Comic Neue:400,500,600,700&display=swap');";
+document.body.appendChild(fnt); 
 
 
 if(!navigator.userAgent.includes('Windows')){
-        var mydiv = document.createElement('div'); 
+    var mydiv = document.createElement('div'); 
 
-        let css_div = ` 
-                height: 40px;
-                width: fit-content;
-                background: grey;
-                border-radius: 10px;
-                padding: 10px;
-                position: absolute;
-                font-family: 'Comic Neue';
-                opacity: 0.7;
-                flex: 1;
-                z-index: 1000;
-                user-select: none;
-        ` 
-        mydiv.setAttribute("style", css_div); 
+    let css_div = ` 
+            height: 40px;
+            width: fit-content;
+            background: grey;
+            border-radius: 10px;
+            padding: 10px;
+            position: absolute;
+            font-family: 'Comic Neue';
+            opacity: 0.7;
+            flex: 1;
+            z-index: 1000;
+            user-select: none;
+    ` 
+    mydiv.setAttribute("style", css_div); 
 
-        var myspeed = document.createElement('span');
-        let css_speed = ` 
-                padding: 2px 10px;
-                background: white;
-                border-radius: 50%;
-                font-size: 30px;
-                z-index: 10000;
-        ` 
+    var myspeed = document.createElement('span');
+    let css_speed = ` 
+            padding: 2px 10px;
+            background: white;
+            border-radius: 50%;
+            font-size: 30px;
+            z-index: 10000;
+    ` 
 
-        myspeed.setAttribute("style", css_speed); 
+    myspeed.setAttribute("style", css_speed); 
 
-        myspeed.innerHTML = document.getElementsByName('media')[0].playbackRate+'x';
-
-
-        var myans = document.createElement('span'); myans.style.display='none';
-        myans.innerHTML = document.getElementsByName('media')[0].playbackRate;
+    myspeed.innerHTML = document.getElementsByName('media')[0].playbackRate+'x';
 
 
-        mydiv.appendChild(myans);
-        mydiv.appendChild(myspeed);
-        document.body.appendChild(mydiv);
+    var myans = document.createElement('span'); myans.style.display='none';
+    myans.innerHTML = document.getElementsByName('media')[0].playbackRate;
 
-        let init, init_speed, speed;
 
-        mydiv.ontouchmove = function(ev) {
-                   var e = ev.targetTouches[0];
+    mydiv.appendChild(myans);
+    mydiv.appendChild(myspeed);
+    document.body.appendChild(mydiv);
 
-                   myans.innerHTML = init_speed + Math.floor(100*(e.pageY-init))/100;
-                   speed = Math.floor(100*(1.005**(eval(myans.innerHTML))))/100;
-                   myspeed.innerHTML = speed+'x';
-                   document.getElementsByName('media')[0].playbackRate = speed;
-                   mydiv.style.transition = 'opacity 0.3s';
-                   }
+    let init, init_speed, speed;
 
-        var dragValue;
+    mydiv.ontouchmove = function(ev) {
+               var e = ev.targetTouches[0];
 
-        mydiv.onmousedown = function(e){
-                dragValue = myans;
-                init = e.pageY;
+               myans.innerHTML = init_speed + Math.floor(100*(e.pageY-init))/100;
+               speed = Math.floor(100*(1.005**(eval(myans.innerHTML))))/100;
+               myspeed.innerHTML = speed+'x';
+               document.getElementsByName('media')[0].playbackRate = speed;
+               mydiv.style.transition = 'opacity 0.3s';
+               }
+
+    var dragValue;
+
+    mydiv.onmousedown = function(e){
+            dragValue = myans;
+            init = e.pageY;
+            init_speed = Number(myans.innerHTML);
+
+
+    document.onmouseup = function(){
+            dragValue = null;
+          }
+
+    document.onmousemove = function(e) {
+               myans.innerHTML = init_speed + Math.floor(100*(e.pageY-init))/100;
+               speed = Math.floor(100*(1.005**(eval(myans.innerHTML))))/100;
+               myspeed.innerHTML = speed+'x';
+               document.getElementsByName('media')[0].playbackRate = speed;
+               mydiv.style.transition = 'opacity 0.3s';
+          };
+
+    }
+
+    var disablescroll = document.createElement('script');
+    disablescroll.src = "https://raunak1089.github.io/all_scripts/disablescroll.js";
+    document.body.appendChild(disablescroll);
+
+        mydiv.ontouchstart = function(ev) {
+            disableScroll();
+                init = ev.targetTouches[0].pageY;
                 init_speed = Number(myans.innerHTML);
-
-
-        document.onmouseup = function(){
-                dragValue = null;
-              }
-
-        document.onmousemove = function(e) {
-                   myans.innerHTML = init_speed + Math.floor(100*(e.pageY-init))/100;
-                   speed = Math.floor(100*(1.005**(eval(myans.innerHTML))))/100;
-                   myspeed.innerHTML = speed+'x';
-                   document.getElementsByName('media')[0].playbackRate = speed;
-                   mydiv.style.transition = 'opacity 0.3s';
-              };
-
         }
 
-        var disablescroll = document.createElement('script');
-        disablescroll.src = "https://raunak1089.github.io/all_scripts/disablescroll.js";
-        document.body.appendChild(disablescroll);
-
-            mydiv.ontouchstart = function(ev) {
-                disableScroll();
-                    init = ev.targetTouches[0].pageY;
-                    init_speed = Number(myans.innerHTML);
-            }
-
-            mydiv.ontouchend = function(ev) {
-                    enableScroll();
-            }
+        mydiv.ontouchend = function(ev) {
+                enableScroll();
+        }
 }
 
 
 // REMEMBER LAST TIME AND SPEED ___________________________ 
 
 try{
-     document.getElementsByName('media')[0].currentTime=localStorage[window.location+'time'];
-     document.getElementsByName('media')[0].playbackRate=localStorage[window.location+'speed'];
+ document.getElementsByName('media')[0].currentTime=localStorage[window.location+'time'];
+ document.getElementsByName('media')[0].playbackRate=localStorage[window.location+'speed'];
 }catch(err){}
 
-
-
-// ARROW KEY FORWARD BACKWARD _____________________________
-
-document.onkeydown=(e)=>{
-    if(e.key=='ArrowRight'){document.getElementsByName('media')[0].currentTime+=5}
-    if(e.key=='ArrowLeft'){document.getElementsByName('media')[0].currentTime-=5}
-}
 
 
 // VLC SPEED CONVENTION ___________________________________
 
 
- const video = document.getElementsByName('media')[0];
- const helloWorld = document.createElement('div');
- helloWorld.innerText = document.getElementsByName('media')[0].playbackRate.toFixed(2)+'x';
- const videoRect = video.getBoundingClientRect();
+const video = document.getElementsByName('media')[0];
+const helloWorld = document.createElement('div');
+helloWorld.innerText = document.getElementsByName('media')[0].playbackRate.toFixed(2)+'x';
+const videoRect = video.getBoundingClientRect();
 
- css_style=`
-  font-size: 3em;
-  display: none;
-  margin: 10px;
-  position: absolute;
-  font-family: 'Comic Neue';
-  font-weight: bold;
-  -webkit-text-fill-color: white;
-  -webkit-text-stroke-width: 1px;
-  -webkit-text-stroke-color: black;
- `;
+css_style=`
+font-size: 3em;
+display: none;
+margin: 10px;
+position: absolute;
+font-family: 'Comic Neue';
+font-weight: bold;
+-webkit-text-fill-color: white;
+-webkit-text-stroke-width: 1px;
+-webkit-text-stroke-color: black;
+`;
 helloWorld.setAttribute('style',css_style);
- helloWorld.style.top = videoRect.top + 10 + 'px';
- helloWorld.style.right = window.innerWidth - videoRect.right + 10 + 'px';
+helloWorld.style.top = videoRect.top + 10 + 'px';
+helloWorld.style.right = window.innerWidth - videoRect.right + 10 + 'px';
 video.parentNode.appendChild(helloWorld);
 
 
 function show_speed(){
-  helloWorld.innerText = document.getElementsByName('media')[0].playbackRate.toFixed(2)+'x';
-  helloWorld.style.display='inline';
-  setTimeout(function() {
-     helloWorld.style.display='none';
-  }, 2000);
+helloWorld.innerText = document.getElementsByName('media')[0].playbackRate.toFixed(2)+'x';
+helloWorld.style.display='inline';
+setTimeout(function() {
+ helloWorld.style.display='none';
+}, 2000);
 }
 
-//addKeyDownListener();
 
-//document.addEventListener('fullscreenchange', function() {
-//  if (document.fullscreenElement) {
-//    document.removeEventListener('keydown');
-//  } else {
-//    addKeyDownListener();
-//  }
-//});
-
-
+// ARROW KEY FORWARD BACKWARD INCLUDED ____________________
 
 document.onkeydown=(e)=>{
+    if(e.key=='ArrowRight'){document.getElementsByName('media')[0].currentTime+=5}
+    if(e.key=='ArrowLeft'){document.getElementsByName('media')[0].currentTime-=5}
     if(e.key=='['){document.getElementsByName('media')[0].playbackRate-=0.1; show_speed();}
     if(e.key==']'){document.getElementsByName('media')[0].playbackRate+=0.1; show_speed();}
 }
@@ -164,8 +149,8 @@ document.onkeydown=(e)=>{
 // REMEMBER LAST TIME AND SPEED ___________________________ 
 
 try{
-     document.getElementsByName('media')[0].currentTime=localStorage[window.location+'time'];
-     document.getElementsByName('media')[0].playbackRate=localStorage[window.location+'speed'];
+ document.getElementsByName('media')[0].currentTime=localStorage[window.location+'time'];
+ document.getElementsByName('media')[0].playbackRate=localStorage[window.location+'speed'];
 }catch(err){}
 
 
@@ -173,6 +158,6 @@ try{
 // STORE LAST PLAYING TIME AND SPEED
 
 setInterval(() => {
-      localStorage.setItem(window.location+'time',document.getElementsByName('media')[0].currentTime); 
-      localStorage.setItem(window.location+'speed',document.getElementsByName('media')[0].playbackRate); 
+  localStorage.setItem(window.location+'time',document.getElementsByName('media')[0].currentTime); 
+  localStorage.setItem(window.location+'speed',document.getElementsByName('media')[0].playbackRate); 
 }, 100)
