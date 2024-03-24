f12519505924bfd441121027ebc8e2f576609904
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2024-03-24 12:54:36 +0530

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -7,7 +7,6 @@ let alertBoxCSS = `
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        color: white;
         padding: 20px;
         border-radius: 5px;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
