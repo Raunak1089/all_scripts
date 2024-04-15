54528d85e61fc3fbbbbecdf86b042afebb6fb9c0
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2024-04-15 20:44:46 +0530

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -2,34 +2,43 @@
 
 let alertBoxCSS = `
 /* Alert Box Styles */
-    .alert-box {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
-        display: none;
-        animation: popMsg 0.5s forwards cubic-bezier(0.61, 1.61, 0.67, 1.15);
-    }
-
-    @keyframes popMsg {
-        from {
-            transform: translate(-50%, -50%) scale(0);
+        .alert-box {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-family: Elsie Swash Caps;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+            display: none;
+            animation: popMsg 0.5s forwards cubic-bezier(0.61, 1.61, 0.67, 1.15);
         }
-        to {
-            transform: translate(-50%, -50%) scale(1);
-        }
-    }
 
-    .alert-box .close-btn {
-        position: absolute;
-        top: 5px;
-        right: 5px;
-        cursor: pointer;
-        font-weight: bold;
-    }
+        @keyframes popMsg {
+            from {
+                transform: translate(-50%, -50%) scale(0);
+            }
+            to {
+                transform: translate(-50%, -50%) scale(1);
+            }
+        }
+
+        .close-btn {
+            position: absolute;
+            top: 5px;
+            right: 5px;
+            cursor: pointer;
+            font-size: larger;
+            font-weight: bold;
+            border-radius: 1em;
+            padding: 3px 5px;
+        }
+
+        .close-btn:hover {
+            background: red;
+            color: white;
+        }
 `;
 
 let alertStyle = document.createElement('style');
@@ -41,7 +50,7 @@ alertBoxDiv.id = 'alertBox';
 alertBoxDiv.classList.add('alert-box');
 alertBoxDiv.innerHTML = `
     <msg>Alert message.</msg>
-    <span class="close-btn" onclick="closeAlert()"><i class="fa-regular fa-xmark"></i></span>
+    <span class="close-btn" onclick="closeAlert()" title="Close"><i class="fa-regular fa-xmark"></i></span>
 `;
 document.body.appendChild(alertBoxDiv);
 
