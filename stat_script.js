805302c28aba429a393ded5a4a57bbac6da1663c
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2024-10-01 14:36:29 +0530

    Update stat_script.js

--- stat_script.js
+++ stat_script.js
@@ -388,6 +388,85 @@ class Fraction_Matrix {
         }
     }
 
+    static rref(A){
+
+        // A = [[5, 1, 1, 3, 7], [2, 2, 4, 8, 1], [4, 3, 3, 7, 2], [6, 9, 7, 6, 1]];
+        // A = [[5, 1, 1, 3], [2, 2, 4, 8], [4, 3, 3, 7]];
+        // A = [[5, 1], [1, 3], [2, 2], [4, 3], [3, 7]];
+        // A = [[1,2,1,0.65],[1,4,3,3.35],[1,2,3,1.75],[1,4,5,4.45],[1,5,6,5.8]];
+    
+        // console.table(A);
+        function mult(arr, n) {
+            let a = [];
+            for (let x of arr) {
+                a.push(Fraction.mult(x, n));
+            }
+            return a;
+        }
+    
+        function row_divide(mat, r, m) {
+            //R1 / m
+            try {
+                let c = [];
+                for (let x of mat[r]) {
+                    c.push(Fraction.divide(x, m));
+                }
+                mat[r] = c;
+            } catch(err) {
+                throw new Error(`Cannot divide ${r}th row of ${mat} with ${m}`)
+            }
+            // console.table(mat.map(row => row.map(element => element.toString())));
+        }
+    
+        function elem_op(mat, r1, r2, m) {
+            // R12(m)
+            let b = [];
+            for (let i = 0; i < mat[0].length; i++) {
+                b.push(Fraction.add(mat[r1][i], mult(mat[r2], m)[i]));
+            }
+            mat[r1] = b;
+            // console.table(mat.map(row => row.map(element => element.toString())));
+        }
+    
+        function interchange(mat, r1, r2) {
+            let c = mat[r1];
+            mat[r1] = mat[r2];
+            mat[r2] = c;
+            // console.table(mat.map(row => row.map(element => element.toString())));
+        }
+    
+        num = Math.min(A.length,A[0].length);
+        turn=0;
+    
+        // ROW ECHLON
+    
+        for (let col = 0; col < num; col++) {
+            if(Number(A[col][col])!=0) {/*console.log(`row_divide(A,${col},A[${col}][${col}])`);*/row_divide(A, col, A[col][col]);}
+            if(Number(A[col][col])==0 && turn <= A.length) {/*console.log(`interchange(A,${col},${A.length-1})`);*/interchange(A, col, A.length-1);col--;turn++;continue}
+            for (let row = col + 1; row < A.length; row++) {
+                // console.log(`elem_op(A,${row},${col},-1*A[${row}][${col}])`);
+                elem_op(A, row, col, Fraction.mult(-1, A[row][col]));
+            }
+        }
+    
+        // REDUCED ROW
+    
+        for (let col = num - 1; col > 0; col--) {
+            for (let row = col - 1; row >= 0; row--) {
+                // console.log(`elem_op(A,${row},${col},-1*A[${row}][${col}])`);
+                elem_op(A, row, col, Fraction.mult(-1, A[row][col]));
+            }
+        }
+    
+    
+    
+    
+        // console.table(A);
+        return A;
+    }
+    
+    
+
     static cofactor(rowId, colId, matrix) {
         const subMatrix = Fraction_Matrix.copyMatrix(matrix).fracObj.filter((_, i) => i !== rowId)
             .map(row => row.filter((_, j) => j !== colId));
@@ -530,7 +609,7 @@ class Matrix {
         // matrix = [[5, 1], [1, 3], [2, 2], [4, 3], [3, 7]];
         // matrix = [[1,2,1,0.65],[1,4,3,3.35],[1,2,3,1.75],[1,4,5,4.45],[1,5,6,5.8]];
     
-        console.table(matrix);
+        // console.table(matrix);
         function mult(arr, n) {
             let a = [];
             for (let x of arr) {
