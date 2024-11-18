bc0f19012961afa4eacb055792eb0e481416ca6e
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2024-11-18 19:43:35 +0530

    Update fontawesome.js

--- fontawesome.js
+++ fontawesome.js
@@ -1,22 +1,32 @@
 const head = document.querySelector('head');
+
+// CSS links for version 6.6.0
 const link1 = document.createElement('link');
 link1.setAttribute('rel', 'stylesheet');
-link1.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.5.1/css/all.css');
+link1.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css');
 head.appendChild(link1);
+
 const link2 = document.createElement('link');
 link2.setAttribute('rel', 'stylesheet');
-link2.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.5.1/css/sharp-thin.css');
+link2.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-duotone-solid.css');
 head.appendChild(link2);
+
 const link3 = document.createElement('link');
 link3.setAttribute('rel', 'stylesheet');
-link3.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.5.1/css/sharp-solid.css');
+link3.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-thin.css');
 head.appendChild(link3);
+
 const link4 = document.createElement('link');
 link4.setAttribute('rel', 'stylesheet');
-link4.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.5.1/css/sharp-regular.css');
+link4.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-solid.css');
 head.appendChild(link4);
+
 const link5 = document.createElement('link');
 link5.setAttribute('rel', 'stylesheet');
-link5.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.5.1/css/sharp-light.css');
+link5.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-regular.css');
 head.appendChild(link5);
 
+const link6 = document.createElement('link');
+link6.setAttribute('rel', 'stylesheet');
+link6.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-light.css');
+head.appendChild(link6);
