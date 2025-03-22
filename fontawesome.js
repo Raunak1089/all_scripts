8f1238dacbf0d9655fb266b2ba3b78cdb34e69b4
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2025-03-22 10:48:47 +0530

    Update to version 6.7.2

--- fontawesome.js
+++ fontawesome.js
@@ -1,32 +1,62 @@
 const head = document.querySelector('head');
 
-// CSS links for version 6.6.0
+// CSS links for version 6.7.2
 const link1 = document.createElement('link');
 link1.setAttribute('rel', 'stylesheet');
-link1.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css');
+link1.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css');
 head.appendChild(link1);
 
 const link2 = document.createElement('link');
 link2.setAttribute('rel', 'stylesheet');
-link2.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-duotone-solid.css');
+link2.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-thin.css');
 head.appendChild(link2);
 
 const link3 = document.createElement('link');
 link3.setAttribute('rel', 'stylesheet');
-link3.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-thin.css');
+link3.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-solid.css');
 head.appendChild(link3);
 
 const link4 = document.createElement('link');
 link4.setAttribute('rel', 'stylesheet');
-link4.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-solid.css');
+link4.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-regular.css');
 head.appendChild(link4);
 
 const link5 = document.createElement('link');
 link5.setAttribute('rel', 'stylesheet');
-link5.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-regular.css');
+link5.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-light.css');
 head.appendChild(link5);
 
 const link6 = document.createElement('link');
 link6.setAttribute('rel', 'stylesheet');
-link6.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-light.css');
+link6.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-thin.css');
 head.appendChild(link6);
+
+const link7 = document.createElement('link');
+link7.setAttribute('rel', 'stylesheet');
+link7.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-solid.css');
+head.appendChild(link7);
+
+const link8 = document.createElement('link');
+link8.setAttribute('rel', 'stylesheet');
+link8.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-regular.css');
+head.appendChild(link8);
+
+const link9 = document.createElement('link');
+link9.setAttribute('rel', 'stylesheet');
+link9.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-light.css');
+head.appendChild(link9);
+
+const link10 = document.createElement('link');
+link10.setAttribute('rel', 'stylesheet');
+link10.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone-thin.css');
+head.appendChild(link10);
+
+const link11 = document.createElement('link');
+link11.setAttribute('rel', 'stylesheet');
+link11.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone-regular.css');
+head.appendChild(link11);
+
+const link12 = document.createElement('link');
+link12.setAttribute('rel', 'stylesheet');
+link12.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone-light.css');
+head.appendChild(link12);
