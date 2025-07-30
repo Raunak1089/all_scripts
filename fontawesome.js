828107366c9bb0f87ae4d742d24f68e10ffbbc4c
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2025-07-30 20:55:00 +0530

    Update fontawesome.js v7.0.0

--- fontawesome.js
+++ fontawesome.js
@@ -1,62 +1,59 @@
-const head = document.querySelector('head');
+document.querySelector('head').innerHTML += `
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/fontawesome.css">
 
-// CSS links for version 6.7.2
-const link1 = document.createElement('link');
-link1.setAttribute('rel', 'stylesheet');
-link1.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css');
-head.appendChild(link1);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/whiteboard-semibold.css">
 
-const link2 = document.createElement('link');
-link2.setAttribute('rel', 'stylesheet');
-link2.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-thin.css');
-head.appendChild(link2);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/thumbprint-light.css">
 
-const link3 = document.createElement('link');
-link3.setAttribute('rel', 'stylesheet');
-link3.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-solid.css');
-head.appendChild(link3);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/slab-press-regular.css">
 
-const link4 = document.createElement('link');
-link4.setAttribute('rel', 'stylesheet');
-link4.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-regular.css');
-head.appendChild(link4);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/slab-regular.css">
 
-const link5 = document.createElement('link');
-link5.setAttribute('rel', 'stylesheet');
-link5.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-duotone-light.css');
-head.appendChild(link5);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-duotone-thin.css">
 
-const link6 = document.createElement('link');
-link6.setAttribute('rel', 'stylesheet');
-link6.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-thin.css');
-head.appendChild(link6);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-duotone-solid.css">
 
-const link7 = document.createElement('link');
-link7.setAttribute('rel', 'stylesheet');
-link7.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-solid.css');
-head.appendChild(link7);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-duotone-regular.css">
 
-const link8 = document.createElement('link');
-link8.setAttribute('rel', 'stylesheet');
-link8.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-regular.css');
-head.appendChild(link8);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-duotone-light.css">
 
-const link9 = document.createElement('link');
-link9.setAttribute('rel', 'stylesheet');
-link9.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-light.css');
-head.appendChild(link9);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-thin.css">
 
-const link10 = document.createElement('link');
-link10.setAttribute('rel', 'stylesheet');
-link10.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone-thin.css');
-head.appendChild(link10);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-solid.css">
 
-const link11 = document.createElement('link');
-link11.setAttribute('rel', 'stylesheet');
-link11.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone-regular.css');
-head.appendChild(link11);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-regular.css">
 
-const link12 = document.createElement('link');
-link12.setAttribute('rel', 'stylesheet');
-link12.setAttribute('href', 'https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone-light.css');
-head.appendChild(link12);
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/sharp-light.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/notdog-duo-solid.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/notdog-solid.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/jelly-fill-regular.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/jelly-duo-regular.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/jelly-regular.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/etch-solid.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/duotone-thin.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/duotone.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/duotone-regular.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/duotone-light.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/thin.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/solid.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/regular.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/light.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/brands.css">
+
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v7.0.0/css/chisel-regular.css">
+`;
