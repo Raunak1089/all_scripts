75aaa994b100c8246c6823eec72a78cf9cf9df2b
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2026-03-25 00:01:24 +0530

    Update fontawesome.js

--- fontawesome.js
+++ fontawesome.js
@@ -1,1 +1,1 @@
-document.querySelector('head').innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">`;
+document.querySelector('head').innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">`;
